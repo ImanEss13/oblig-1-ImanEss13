cd806adf546c73ec1d5129c581a259b4d0400655
ImanEss13 <s376827@oslomet.no>
2025-03-13 15:05:20 +0100

    changed structure

---
+++ newscript.js
@@ -0,0 +1,107 @@
+//TODO: add on load/ready functionality
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("ready!");
+    // TODO: add functionality to change password
+    /*document.getElementById('change-password-form').addEventListener('submit', function(event) {
+       event.preventDefault(); // Hindrer at skjemaet sender og oppdaterer siden
+    
+       // Hent verdiene fra inputfeltene
+       const newPassword = document.getElementById('new-password').value;
+       const confirmPassword = document.getElementById('confirm-password').value;
+    
+       // Valider at passordene er like
+       if (newPassword === confirmPassword) {
+           // Hvis passordene er like, vis en suksessmelding
+           document.getElementById('password-feedback').innerText = "Password changed successfully!";
+           document.getElementById('password-feedback').style.color = "green";
+       } else {
+           // Hvis passordene ikke er like, vis en feilmelding
+           document.getElementById('password-feedback').innerText = "Passwords do not match.";
+           document.getElementById('password-feedback').style.color = "red";
+       }
+    }); */
+    });
+    $(".book").click(function () {
+       var bookId = $(this).attr("id") + "-book"; // Finn bokens ID
+       $(".spell-page").addClass("hidden"); // Skjul alle åpne bøker
+        $("#" + bookId).removeClass("hidden"); // Vis den nye boken
+    });
+    
+    /*$("#close-tome").click(function () {
+       $(".spell-page").addClass("hidden"); // Skjul bokinnhold
+    });*/
+    // 
+    /*document.getElementById('rename-form').addEventListener('submit', function(event) {
+       event.preventDefault(); // Hindrer at skjemaet sender og oppdaterer siden
+    
+       // Hent verdiene fra inputfeltene
+       const newName = document.getElementById('new-name').value;
+       const newTitle = document.getElementById('new-title').value;
+    
+       document.getElementById('wizard-name').innerText = newName;
+        document.getElementById('wizard-title').innerText = newTitle;
+       
+       // Oppdater navnet og tittelen på siden
+       document.getElementById('wizard-name-display').innerText = newName;
+       document.getElementById('wizard-title-display').innerText = newTitle;
+       document.getElementById('name-title').innerHTML = newTitle;
+       
+       // Vis en suksessmelding
+       document.getElementById('name-feedback').innerText = "Name and title changed successfully!";
+       document.getElementById('name-feedback').style.color = "green";
+    });*/
+    
+    //TODO: add functionality to open the correct spell books
+    function openSpellBook(){
+       console.log("How do you find the right spellbook?!")
+    
+    }
+    /*document.getElementById('summon-form').addEventListener('submit', function(event) {
+       event.preventDefault(); // Hindrer at skjemaet sender og oppdaterer siden
+    
+       // Hent verdier fra skjemaet
+       const familiarName = document.getElementById('familiar-name').value;
+       const familiarType = document.getElementById('familiar-type').value;
+       const hasWings = document.getElementById('has-wings').checked;
+       const wingType = hasWings ? document.getElementById('wing-type').value : 'None';
+       const traits = Array.from(document.querySelectorAll('input[name="traits"]:checked')).map(el => el.value);
+       const mood = document.getElementById('familiar-mood').value;
+       const contractEnd = document.getElementById('contract-end').value;
+       
+    
+    
+    
+       // DETTE MÅ KONVERTERES TIL JQUERY 
+       // Bygg en beskrivelse av familiaren
+       let description = `${familiarName}, a ${familiarType} with ${wingType} wings.`;
+       if (traits.length > 0) {
+          description += ` It has the following traits: ${traits.join(', ')}.`;
+       }
+       description += ` Mood: ${mood}. Contract ends on: ${contractEnd}.`;
+    document.getElementById('familiar-feedback').innerText = description;
+    
+       // Vis familiarens beskrivelse i konsollen
+       console.log(description);
+       alert(description);// Nå sender vi rikitg beskrivelse til alert
+    
+       // Vis en tilbakemelding på nettsiden
+       document.getElementById('familiar-feedback').innerText = "Familiar summoned successfully!";
+       document.getElementById('familiar-feedback').style.color = "green";
+    }); */
+    
+    // Dynamisk visning av vingetype basert på "Has Wings"-checkbox
+    /*document.getElementById('has-wings').addEventListener('change', function() {
+       const wingTypeField = document.getElementById('wing-type');
+       if (this.checked) {
+           wingTypeField.classList.remove('hidden');
+       } else {
+           wingTypeField.classList.add('hidden');
+       }
+    });*/
+       
+    
+    //TODO: add functionality to change password
+    //TODO: add functionality to change name
+    //TODO: add functionality to summon familiar
+    // have a function where you can load content into a "user array" -> easier to work with
+    
--- script.js
+++ script.js
@@ -1,94 +1,94 @@
 //TODO: add on load/ready functionality
 document.addEventListener("DOMContentLoaded", () => {
 console.log("ready!");
-// TODO: add functionality to change password
-document.getElementById('change-password-form').addEventListener('submit', function(event) {
-   event.preventDefault(); // Hindrer at skjemaet sender og oppdaterer siden
 
-   // Hent verdiene fra inputfeltene
-   const newPassword = document.getElementById('new-password').value;
-   const confirmPassword = document.getElementById('confirm-password').value;
-
-   // Valider at passordene er like
-   if (newPassword === confirmPassword) {
-       // Hvis passordene er like, vis en suksessmelding
-       document.getElementById('password-feedback').innerText = "Password changed successfully!";
-       document.getElementById('password-feedback').style.color = "green";
-   } else {
-       // Hvis passordene ikke er like, vis en feilmelding
-       document.getElementById('password-feedback').innerText = "Passwords do not match.";
-       document.getElementById('password-feedback').style.color = "red";
-   }
-});
 });
 $(".book").click(function () {
    var bookId = $(this).attr("id") + "-book"; // Finn bokens ID
-   $("#" + bookId).removeClass("hidden"); // Vis boken
    $(".spell-page").addClass("hidden"); // Skjul alle åpne bøker
     $("#" + bookId).removeClass("hidden"); // Vis den nye boken
 });
 
-$(".close-tome").click(function () {
-   $(".spell-page").addClass("hidden"); // Skjul bokinnhold
-});
-// 
-document.getElementById('rename-form').addEventListener('submit', function(event) {
-   event.preventDefault(); // Hindrer at skjemaet sender og oppdaterer siden
-
-   // Hent verdiene fra inputfeltene
-   const newName = document.getElementById('new-name').value;
-   const newTitle = document.getElementById('new-title').value;
-   
-   // Oppdater navnet og tittelen på siden
-   document.getElementById('wizard-name').innerText = newName;
-   document.getElementById('wizard-title').innerText = newTitle;
-   
-   // Vis en suksessmelding
-   document.getElementById('name-feedback').innerText = "Name and title changed successfully!";
-   document.getElementById('name-feedback').style.color = "green";
-});
 
 //TODO: add functionality to open the correct spell books
 function openSpellBook(){
    console.log("How do you find the right spellbook?!")
 
 }
-document.getElementById('summon-form').addEventListener('submit', function(event) {
-   event.preventDefault(); // Hindrer at skjemaet sender og oppdaterer siden
 
-   // Hent verdier fra skjemaet
-   const familiarName = document.getElementById('familiar-name').value;
-   const familiarType = document.getElementById('familiar-type').value;
-   const hasWings = document.getElementById('has-wings').checked;
-   const wingType = hasWings ? document.getElementById('wing-type').value : 'None';
-   const traits = Array.from(document.querySelectorAll('input[name="traits"]:checked')).map(el => el.value);
-   const mood = document.getElementById('familiar-mood').value;
-   const contractEnd = document.getElementById('contract-end').value;
-   
-   // Bygg en beskrivelse av familiaren
-   let description = `${familiarName}, a ${familiarType} with ${wingType} wings.`;
-   if (traits.length > 0) {
-      description += ` It has the following traits: ${traits.join(', ')}.`;
+$(document).ready(function () {
+   // Klikk på bokomslagene for å vise de respektive spell-sidene
+   $('#change-password-book-cover').click(function () {
+       $('#change-password-book').removeClass('hidden');
+       $('#book-view').removeClass('hidden');
+   });
+
+   $('#change-name-book-cover').click(function () {
+       $('#change-name-book').removeClass('hidden');
+       $('#book-view').removeClass('hidden');
+   });
+
+   $('#summon-familiar-book-cover').click(function () {
+       $('#summon-familiar-book').removeClass('hidden');
+       $('#book-view').removeClass('hidden');
+   });
+
+   // Lukk boken
+   $('#close-tome').click(function () {
+       $('#book-view').addClass('hidden');
+   });
+
+   // Formulær for å endre passord
+   $('#change-password-form').submit(function (e) {
+       e.preventDefault();
+       var newPassword = $('#new-password').val();
+       var confirmPassword = $('#confirm-password').val();
+
+       if (newPassword === confirmPassword) {
+           $('#password-feedback').text('Password changed successfully!');
+       } else {
+           $('#password-feedback').text('Passwords do not match.');
+       }
+   });
+
+   // Formulær for å endre navn
+   $('#rename-form').submit(function (e) {
+       e.preventDefault();
+       var newName = $('#new-name').val();
+       var newTitle = $('#new-title').val();
+
+       $('#wizard-name').text(newName);
+       $('#wizard-title').text(newTitle);
+
+       $('#name-feedback').text('Name and title updated!');
+       $('#name-title').html(newTitle);
+   });
+
+   // Formulær for å påkalle familiar
+$('#summon-form').submit(function (e) {
+   e.preventDefault();
+   var familiarName = $('#familiar-name').val();
+   var familiarType = $('#familiar-type').val();
+   var hasWings = $('#has-wings').is(':checked');
+   var familiarMood = $('#familiar-mood').val();
+   var contractEnd = $('#contract-end').val();
+
+   var familiarDetails = 'Summoning: ' + familiarName + ' the ' + familiarType + ' with a mood of ' + familiarMood;
+   if (hasWings) {
+       var wingType = $('#wing-type').val();
+       familiarDetails += ' and wings (' + wingType + ')';
    }
-   description += ` It appears to be ${mood}. The contract ends on ${contractEnd}.`;
-
-   // Vis familiarens beskrivelse i konsollen
-   console.log(description);
-   alert(Description);// Nå sender vi rikitg beskrivelse til alert
-
-   // Vis en tilbakemelding på nettsiden
-   document.getElementById('familiar-feedback').innerText = "Familiar summoned successfully!";
-   document.getElementById('familiar-feedback').style.color = "green";
+   alert(familiarDetails);
 });
 
-// Dynamisk visning av vingetype basert på "Has Wings"-checkbox
-document.getElementById('has-wings').addEventListener('change', function() {
-   const wingTypeField = document.getElementById('wing-type');
-   if (this.checked) {
-       wingTypeField.classList.remove('hidden');
-   } else {
-       wingTypeField.classList.add('hidden');
-   }
+   // Vis/hide wing type input
+   $('#has-wings').change(function () {
+       if ($(this).is(':checked')) {
+           $('#wing-type').removeClass('hidden');
+       } else {
+           $('#wing-type').addClass('hidden');
+       }
+   });
 });
    
 
